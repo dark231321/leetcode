d1b31ce16cea4e3e479ac31ff83f7f31181bc4cc
vtb4050112 <vtb4050112@region.vtb.ru>
2022-02-03 04:49:51 +0400

    some tasks

---
+++ src/main/java/org/example/leetcode/other/string/PermutationStrings.java
@@ -0,0 +1,30 @@
+package org.example.leetcode.other.string;
+
+import java.util.Arrays;
+
+public class PermutationStrings {
+
+    public static boolean isPermutationsStrings(String first, String sec) {
+        if (first.length() != sec.length()) {
+            return false;
+        }
+
+        char[] firstArr = first.toCharArray();
+        char[] secArr = sec.toCharArray();
+        Arrays.sort(firstArr);
+        Arrays.sort(secArr);
+
+        for (int i = 0; i < firstArr.length; i++) {
+            if (firstArr[i] != secArr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPermutationsStrings("First", "tsriF"));
+        System.out.println(isPermutationsStrings("Fi", "tsriF"));
+        System.out.println(isPermutationsStrings("Fi", "tsriF"));
+    }
+}
---
+++ src/main/java/org/example/leetcode/other/string/UniqueChars.java
@@ -0,0 +1,21 @@
+package org.example.leetcode.other.string;
+
+public class UniqueChars {
+
+    // 0000 0000 0000 0000 0000 0000 0000 0000
+    public static Boolean isUnique(String value) {
+        int isUniqueBitMassive = 0;
+        for (char el: value.toCharArray()) {
+            int pos = el - 'a';
+            if ((isUniqueBitMassive >> pos) % 2 != 0) {
+                return true;
+            }
+            isUniqueBitMassive |= (1 << pos);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isUnique("abota"));
+    }
+}
