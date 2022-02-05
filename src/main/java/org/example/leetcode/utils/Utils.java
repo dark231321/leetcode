231e099df4551a3e60348f991f9dc3fe0819ee5b
vtb4050112 <vtb4050112@region.vtb.ru>
2022-02-05 04:40:02 +0400

    some tasks

---
+++ src/main/java/org/example/leetcode/utils/Utils.java
@@ -0,0 +1,34 @@
+package org.example.leetcode.utils;
+
+public class Utils {
+
+    public static int randomInt(int n) {
+        return (int) (Math.random() * n);
+    }
+
+    public static int randomIntInRange(int min, int max) {
+        return randomInt(max + 1 - min) + min;
+    }
+
+    public static int[][] randomMatrix(int M, int N, int min, int max) {
+        int[][] matrix = new int[M][N];
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                matrix[i][j] = randomIntInRange(min, max);
+            }
+        }
+        return matrix;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(" " + matrix[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("---------------");
+    }
+
+
+}
